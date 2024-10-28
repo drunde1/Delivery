00cd4005f23de3ddcbbd57b0d5dd8d3b14da69fd
drunde1 <androxa228@gmail.com>
2024-10-28 21:49:52 +0300

    fin

--- Delivery.Application/obj/Debug/net8.0/Delivery.Application.AssemblyInfo.cs
+++ Delivery.Application/obj/Debug/net8.0/Delivery.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delivery.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c11b490e3f32e5c2bfe51e66158b9211fb12cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+087b19e930f081aea4fdc20223aff9fe192b7a34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delivery.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Delivery.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
